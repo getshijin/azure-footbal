b097e50ef768f664d55227cc556c71196e9fe789
Shijin Mohanan Sheela <getshijin@gmail.com>
2025-03-08 21:27:01 +0100

    version6

--- dags/wikipedia_flow.py
+++ dags/wikipedia_flow.py
@@ -1,8 +1,12 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
-from pipeline.wikipedia_pipeline import get_wikipedia_page
 
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pipelines.wikipedia_pipeline import extract_wikipedia_data
 dag =DAG(
     dag_id='wikipedia_flow',
     default_args={
@@ -16,7 +20,7 @@ dag =DAG(
 #Extraction from wikipedia
 extract_data_from_wikipedia = PythonOperator(
     task_id = "extract_data_from_wikipedia",
-    python_callable = get_wikipedia_page,
+    python_callable = extract_wikipedia_data,
     provide_context = True,
     op_kwargs = {"url": "https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_capacity"},
     dag= dag
--- pipelines/wikipedia_pipeline.py
+++ pipelines/wikipedia_pipeline.py
@@ -11,3 +11,35 @@ def get_wikipedia_page(url):
         return response.text
     except requests.RequestException as e:
         print(f"An error occured: {e}")
+
+def get_wikipedia_data(html):
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find_all("table",{"class": "wikitable sortable"})[0]
+
+    table_rows = table.find_all('tr')
+    return table_rows
+
+def extract_wikipedia_data(**kwargs):
+    url = kwargs['url']
+    html = get_wikipedia_page(url)
+    rows = get_wikipedia_data(html)
+
+    data = []
+
+    for i in range(1,len(rows)):
+        tds = rows[i].find_all('td')
+        values = {
+            'rank': i,
+            'stadium': tds[0].text,
+            'capacity': tds[1].text,
+            'region': tds[2].text,
+            'country': tds[3].text,
+            'city': tds[4].text,
+            'images': tds[5].find('img').get('src').split("//")[1] if tds[5].find('img') else 'NO-IMAGE',
+            'home_team': tds[6].text
+        }
+        data.append(values)
+    print(data)
+    return data
